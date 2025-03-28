1e1f3cdbdd609f50dc9e5b2d54723671debd4f2b
JustinLin0121 <chenhao.lin0121.com>
2025-03-28 15:57:21 +0800

    feat: 增加人移動邊界 球上下碰撞

--- game/script.js
+++ game/script.js
@@ -1,31 +1,40 @@
 var player = document.getElementById("pikacho1");
+var ball = document.getElementById("ball");
+var g = 1.2; //重力加速度
+
+
 var posY = parseInt(window.getComputedStyle(player).bottom);
 var posX = parseInt(window.getComputedStyle(player).left);
-var velocityY = 0; // Initial velocity
-var velocityX = 0; // Initial velocity
-var g = 1.2; // Gravity
-var jumpPower = 25; // Power of the jump
-var jumpPower2 = 20;
+var velocityY = 0; //Y速度
+var velocityX = 0; //X速度
+var jumpPower = 25; //彈跳力1
+var jumpPower2 = 20; //彈跳力2
 var isJumping = false;
 var isJumping2 = false;
 var movingRight = false;
 var movingLeft = false;
 
-// 按下右箭头键时开始向右移动
+
+var ballY = parseInt(window.getComputedStyle(ball).bottom);
+var ballX = parseInt(window.getComputedStyle(ball).left);
+var ballVY = 0;
+var ballVX = 0;
+
+
+
 document.addEventListener('keydown', function (event) {
-    if (event.key === "ArrowRight" && !movingRight) {
-        movingRight = true;  // 标记为按住右箭头
+    if (event.key === "ArrowRight" && !movingLeft) {
+        movingRight = true;
     } else if (event.key === "ArrowLeft" && !movingRight) {
-        movingLeft = true;  // 标记为按住右箭头
+        movingLeft = true;
     }
 });
 
-// 松开右箭头键时停止向右移动
 document.addEventListener('keyup', function (event) {
     if (event.key === "ArrowRight") {
-        movingRight = false;  // 标记为没有按住右箭头
+        movingRight = false;
     } if (event.key === "ArrowLeft") {
-        movingLeft = false;  // 标记为没有按住右箭头
+        movingLeft = false;
     }
 });
 
@@ -42,24 +51,37 @@ document.addEventListener("keydown", function (event) {
 function update() {
 
     if (isJumping) {
-        velocityY -= g; // Gravity pulls the player down
-        posY += velocityY; // Update position based on velocity
-
+        velocityY -= g;
+        posY += velocityY;
     }
-    player.style.bottom = posY + "px"; // Update the player's position
+    player.style.bottom = posY + "px";
+
     if (posY <= 0) {
         player.style.bottom = "0px";
         velocityY = 0;
         isJumping = false;
         isJumping2 = false;
     }
-
-    if (movingRight) {
-        posX += 10; // Update position based on velocity
+    if (movingRight && posX < 540) {
+        posX += 10;
     }
-    if (movingLeft) {
-        posX -= 10; // Update position based on velocity
+    if (movingLeft && posX > 0) {
+        posX -= 10;
     } player.style.left = posX + "px";
+
+
+    ballVY -= g;
+    ballY += ballVY;
+    if (ballY <= 0) {
+        ballY = 0;
+        ballVY *= -1;
+    } else if (ballY >= 530) {
+        ballY = 530;
+        ballVY *= -1;
+    }
+    ball.style.bottom = ballY + "px";
+
+
 
     requestAnimationFrame(update);
 }
